56c42126bad34dec2c596d522ee7a5d32dc6c516
TikendraDeshmukh <tikendradeshmukhid@gmail.com>
2024-09-25 12:14:22 -0700

    19tthhhhh cooommit

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import { About, Footer, Header, Testimonial,Work } from './container';
-import { Navbar} from './components';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
-const App = () => {
-  return (
-    <div className="app">
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <Footer />
-    </div>
-  )
-}
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
